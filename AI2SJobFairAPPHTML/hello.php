01c6bb0769f717a4f0717a73a1cd9908dbd99171
cyberguli <guglielmo.padula@aruba.it>
2023-03-25 13:26:34 +0100

    try to add a server infrastructure

---
+++ AI2SJobFairAPPHTML/hello.php
@@ -0,0 +1,7 @@
+if(!empty($_POST['data'])){
+$data = $_POST['data'];
+$fname =  "hello_2.csv";//generates random name
+$file = fopen("upload/" .$fname, 'w');//creates new file
+fwrite($file, $data);
+fclose($file);
+}
